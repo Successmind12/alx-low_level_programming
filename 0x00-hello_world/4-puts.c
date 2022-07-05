98cf4710db4af1e89e3e7872e51c94b11d880a6d
Successmind12 <airatsuccess@gmail.com>
2022-07-04 17:04:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
 
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
-}
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+ int main(void)
+
+	 {
+
+		 	puts("\"Programming is like building a multilingual puzzle\n");
+
+			return (0);
+
+	 }
+
+
