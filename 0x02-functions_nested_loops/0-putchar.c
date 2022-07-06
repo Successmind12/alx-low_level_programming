cab507531177559f76f8b95b0d36436f2b1ca719
Successmind12 <airatsuccess@gmail.com>
2022-07-05 18:38:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAXSTRING 40
+
+int main(void){
+
+	char message[]= "_putchar\n";
+
+	for (int count = 0; count < MAXSTRING; count++)
+
+	{ 
+
+			if (message[count] == '\0')
+
+					{
+
+				         putchar('\n');
+                                                       
+					 break;
+                                        }
+
+				else               
+                                                    
+			        puchar(message[count]);
+
+	}
+
+	return 0;
