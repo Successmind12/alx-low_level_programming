0ca842dae3ef894d60075e18b542d469d70861a4
Successmind12 <airatsuccess@gmail.com>
2022-07-06 07:24:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+int main()
+
+{	
+
+		/* Get the character to be writtten */
+
+		char ch = 'a';
+
+	       /* Write the character to stdout */
+
+	        for (ch = 'a'; ch <= 'z'; ch++ )
+                putchar(ch);
+                putchar('\n');
+                return (0);
+
+} 
