b6eb9cf573f83f3ad2b5bafa6ef4e600f6877f3b
Successmind12 <airatsuccess@gmail.com>
2022-07-06 23:07:04 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+
+
+/**
+ * print_sign - check numbers
+ * @n : number to check
+ * Return: 0 or 1
+ */
+
+
+
+int print_sign(int n)
+
+{
+
+	int result;
+
+
+
+	if (n > 0)
+
+	{
+
+		 result = 1;
+
+		  _putchar('+');
+
+	}
+
+	else if (n == 0)
+
+	{
+
+		 result = 0;
+
+		  _putchar('0');
+
+	}
+
+	else
+
+	{
+
+		 result = -1;
+
+		  _putchar('-');
+
+	}
+
+
+
+	return (result);
+
+}
+
+
