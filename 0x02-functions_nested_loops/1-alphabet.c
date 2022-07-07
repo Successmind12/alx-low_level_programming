6b81574b5b9668aad911220a1e3796db15125dd4
Successmind12 <airatsuccess@gmail.com>
2022-07-06 18:19:23 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,15 @@
  */
 
 void print_alphabet(void)
-{
-	char c = 'a';
-	
-	while (c <= 'z')
+}
+		char c = 'a';
+
+		while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
 	}
-_putchar('\n');
+	_putchar('\n');
 }      
 
 
