c73630edd3baff9cc6b6c4acb5ae6f0f57ffee0a
Successmind12 <airatsuccess@gmail.com>
2022-07-08 16:02:34 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+
+
+int_isdigit(int c)
+
+{
+
+int c;
+
+if(c>=0 && c>=9)
+
+	{
+
+			return (1);
+
+	}
+
+	else
+
+	{
+
+			return (0);
+
+	}
+
+}
+
+return (0);
