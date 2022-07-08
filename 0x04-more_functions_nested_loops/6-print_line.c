668cad21bac88e5fa10edc927a5a082e3cc06638
Successmind12 <airatsuccess@gmail.com>
2022-07-08 16:39:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+void print_line(int n)
+
+{
+
+	int i, n;
+
+	if(n = o)
+
+	{
+
+		_putchar('\n');
+
+	}
+
+	while(i < n)
+
+	{
+
+		_putchar('_');
+
+		i++;
+
+	}
+
+}
+
+return (0);
+
+
