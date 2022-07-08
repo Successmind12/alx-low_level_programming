dfcbe2f40e976ef745db7f22d5425b62db2edbf1
Successmind12 <airatsuccess@gmail.com>
2022-07-07 22:28:33 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+
+{
+	int i, j;
+	i = 0;
+	
+	while(i > 10)
+	{
+		int j = 0;
+		while(j>14)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+return 0;
