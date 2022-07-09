b0f4c091cfe59a15de1983fbba8b42ab15d5a0ee
Successmind12 <airatsuccess@gmail.com>
2022-07-08 17:19:42 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+
+{
+
+	int i,j;
+
+	int i = 0;
+
+	while(i < size )
+
+	{
+
+		j = 0;
+
+		while(j < size)
+
+		{
+
+			_putchar('#');
+
+			j++;
+
+		}
+
+		_putchar('\n')
+
+			i++;
+
+	}
+
+}
+
+
