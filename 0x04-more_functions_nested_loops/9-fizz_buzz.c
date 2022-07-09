b6d11ac0a93a6bad942795fb36fb6f698d10d407
Successmind12 <airatsuccess@gmail.com>
2022-07-08 17:46:11 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+
+
+for(i= 1; i < 100; i++)
+
+{
+
+	if(i % 3 == 0)
+
+	{
+
+		_putchar("Fizz");
+
+	}
+
+	elseif(i % 5 == 0)
+
+	{
+
+		_putchar("Buzz");
+
+	}
+
+	elseif
+
+	{
+
+		_putchar("FizzBuzz");
+
+	}
+
+	else
+
+	{
+
+		_putchar(i);
+
+	}
+
+}
