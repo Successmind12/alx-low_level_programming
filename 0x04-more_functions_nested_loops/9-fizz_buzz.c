aef739cd7c6d82844564d48f6a265638bfda386f
Successmind12 <airatsuccess@gmail.com>
2022-07-11 15:16:46 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,34 @@
 
 int main(void)
 {
-int i = 1;
+int i;
 
-for (;  i < 100 ; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz ");
-else if (i % 3 == 0)
-printf("Fizz ");
-else if (i % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", i);
+if (i % 5 == 0 && i % 3 == 0)
+{
+printf("%s", "FizzBuzz");
 }
-print("Buzz\n");
+else if (i % 3 == 0)
+{
+printf("%s", "Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("%s", "Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
 
 return (0);
 }
+
+
