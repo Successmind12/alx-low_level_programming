4f22388f99395c2e20cf49f67aaa32fd9d97d39b
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2022-07-14 00:47:47 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the values it points to 98
- * @*n: pointer that sets variable
+ * @n: pointer that sets variable
  */
 
 void reset_to_98(int *n)
