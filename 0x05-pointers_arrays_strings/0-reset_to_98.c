9899e55a699bb76fed8bd7902fb27aa7ef692f85
Successmind12 <airatsuccess@gmail.com>
2022-07-12 17:37:39 -0700

    1-isdigit.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * function that takes a pointer to an int as parameter
+ * @n: input parameters n for function reset_to_98
+ * void reset_to_98 is the function required
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
+
+
