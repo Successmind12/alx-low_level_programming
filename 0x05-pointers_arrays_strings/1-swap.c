1c08ab0f148a8a5856c064ac918aedc3d235677c
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2022-07-14 00:53:48 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/** 
+/**
  * function that swaps the values of two integers.
  * @n: input paramters required, int*a, int *b
  * Return: Always 0.
- */ 
+ */
 
 void swap_int(int *a, int *b)
 {
