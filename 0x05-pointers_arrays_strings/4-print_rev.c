074f041a4693a8d4433a1299af3c23275a812dfc
Successmind12 <airatsuccess@gmail.com>
2022-07-13 14:09:02 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * function that prints a string, in reverse
+ * followed by a new line
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+int i, n;
+
+for ( i = 0; s[i] != '\0'; i++)
+{
+
+}
+
+for (n = i - 1; n >= 0; n--)
+{
+_putchar(s[n]);
+_putchar('\n');
+}
+
+
+
+
