bce706a9249363002a7d4b8f175f7cfee53a2249
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2022-07-14 04:55:26 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,23 +2,22 @@
 #include <stdio.h>
 
 /**
- * function that prints n elements of an array of integers
- * @n: input parameters are int *a, int n
- * void print_array(int *a, int n) is the function used
- * Return: Always 0.
+ * print_array: function that prints n elements of an array of integers
+ * @n: used to print comma and space
+ * @a: used to pointer that holds value
  */
 
 void print_array(int *a, int n)
 {
 int i;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 
 	{
 
 		printf("%d", a[i]);
 
-		if (i != n-1)
+		if (i != n - 1)
 
 		{
 
