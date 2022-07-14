02b386d466fb42f2d2fe393ecf1eccccf46669ad
Successmind12 <airatsuccess@gmail.com>
2022-07-14 16:26:55 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** 
+ *_strncat - program that concantenates two strings
+ *@dest: string destination
+ *@src: string source
+ *@n: number of bytes
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; n > i; i++)
+{
+dest[i] = '\0';
+}
+
+return (dest);
+}
