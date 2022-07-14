a50180d17b3c1e0cd9283ea94dc2b9e1380288fe
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2022-07-14 03:12:33 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,8 +3,7 @@
 /**
  * function that prints every other character of a string
  * starting with the first character, followed by a new line
- * @n: input parameters char *str
- * Return: Always 0.
+ * @str: input parameters char *str
  */
 
 void puts2(char *str)
