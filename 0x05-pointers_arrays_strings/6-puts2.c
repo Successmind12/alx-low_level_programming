7c9b1f883fffb3f2a0681417fb00c7edb72dbd9c
Successmind12 <106842379+Successmind12@users.noreply.github.com>
2022-07-14 06:23:09 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,15 @@
 
 void puts2(char *str)
 {
-    int i;
-    for ( i = 0; str[i] != '\0'; i++)
-    {
-        if (i % 2 == 0)
-        {
-            _putchar(str[i]);
-        }  
-    }
-    _putchar('\n');
+int i;
+
+for ( i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
 }
 
