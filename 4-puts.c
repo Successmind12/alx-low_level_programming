2f52d9df4f601af7596db1ab8ad4e5b78a04debe
Successmind12 <airatsuccess@gmail.com>
2022-07-24 18:30:48 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - the main function
+ *Return: void
+ */
+
+int main(void)
+{
+char str[70];
+
+char str[70] = "Programming is like writng a multilingual language";
+puts(str);
+
+return (0);
+}
+
+
