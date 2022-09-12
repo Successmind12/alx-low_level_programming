613a309c8dcd2e255ab20a95a1d74859945d4cfa
Successmind12 <airatsuccess@gmail.com>
2022-09-12 16:36:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,12 +7,6 @@
 
 int main(void)
 {
-char s[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-int i = 0;
-while (s[i] != '\n' && s[i] != '\0')
-{
-putchar(s[i]);
-i++;
-}
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
