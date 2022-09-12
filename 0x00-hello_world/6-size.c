07910b7002adc6e4fa397e1fa55ef28ee259838e
Successmind12 <airatsuccess@gmail.com>
2022-09-12 08:26:56 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ long int b;
 long long int d;
 float f;
 
-printf("Size of a char: %d bytes(s) \n", sizeof(c));
-printf("Size of a int: %d bytes(s) \n", sizeof(a));
-printf("Size of a long int: %d bytes(s) \n", sizeof(b));
-printf("Size of a long long int: %d bytes(s) \n", sizeof(d));
-printf("Size of a float: %d bytes(s) \n", sizeof(f));
+printf("Size of a char: %d bytes(s)\n", sizeof(c));
+printf("Size of an int: %d bytes(s)\n", sizeof(a));
+printf("Size of a long int: %d bytes(s)\n", sizeof(b));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(d));
+printf("Size of a float: %d bytes(s)\n", sizeof(f));
 }
