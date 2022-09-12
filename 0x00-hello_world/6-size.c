3fff9b69692dc782d4c3f4ece15657824a419842
Successmind12 <airatsuccess@gmail.com>
2022-09-12 15:22:57 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,4 +19,5 @@ printf("Size of an int: %d byte(s)\n", sizeof(a));
 printf("Size of a long int: %d byte(s)\n", sizeof(b));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(f));
+return(0);
 }
