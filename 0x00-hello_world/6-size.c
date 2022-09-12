235e0ed369e23500e35f046f407ed6760fe103af
Successmind12 <airatsuccess@gmail.com>
2022-09-12 08:06:02 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+ * main - the main function
+ * Return: void
+ */
+
+int main(void)
+
+{
+int a;
+long int b;
+long long int d;
+float f;
+
+printf("Size of a char: %d bytes(s) \n", sizeof(c));
+printf("Size of a int: %d bytes(s) \n", sizeof(a));
+printf("Size of a long int: %d bytes(s) \n", sizeof(b));
+printf("Size of a long long int: %d bytes(s) \n", sizeof(d));
+printf("Size of a float: %d bytes(s) \n", sizeof(f));
+}
