66ba0768ea47dc35bd38b6d5a746d27e57aefcda
Successmind12 <airatsuccess@gmail.com>
2022-09-13 06:43:17 +0530

    .\101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * main - the main function
+ * Return: void
+ */
+
+int main()
+{
+    fprintf(stderr, "and that piece of art is useful\"" );
+    fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
+    return (0);
+}
