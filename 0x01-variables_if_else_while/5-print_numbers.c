b1e7bf7b04f5f4a39c111e85ca0c119758bb3834
Airat <airatsuccess@gmail.com>
2023-02-17 14:49:22 -0800

    5-print_numbers.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers between 0 and 9
+ * Description - main value
+ * followed by new line
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for ( i = 0; i < 10; i++)
+{
+    printf("%d",i);
+}
+putchar('\n');
+
+return (0);
+}
