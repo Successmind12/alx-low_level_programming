7ed3258976f94adb9480888c28ad80726f85cc73
Airat <airatsuccess@gmail.com>
2023-02-17 21:20:35 -0800

    100-print_comb3.c && git push
    git add . && git commit -m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,28 +11,24 @@
 */
 int main(void)
 {
-int a = '0';
 int i;
-for(i = 1; i < 100; i++)
+for (i = 1; i < 100; i++)
 {
-    int v = ',';
-    int d = ' ';
-    int f = (i/10)+'0';
-    int g = (i%10)+'0';
-   
-    if((g < f) || (g == f))
-    continue;
-        {
-            putchar(f);
-            putchar(g);
-    
-                if(i != 89)
-                    {
-                        putchar(v);
-                        putchar(d);
-        
-                    }
-        }
+int v = ',';
+int d  = ' ';
+int f = (i/10)+'0';
+int g = (i%10)+'0';
+if ((g < f) || (g == f))
+continue;
+{
+putchar(f);
+putchar(g);
+if(i != 89)
+{
+putchar(v);
+putchar(d);
+}
+}
 }
 return (0);
 }
