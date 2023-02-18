300d2edd8d5a2293f541767ed33701b9c8e63161
Airat <airatsuccess@gmail.com>
2023-02-17 20:46:08 -0800

    7-print_tebahpla.c && git push
    git add . && git commit -m

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,10 +13,10 @@
 int main(void)
 {
 char letter;
-    for (letter = 122; letter >= 97; letter--)
-    {
-        putchar(letter);
-    }
+for (letter = 122; letter >= 97; letter--)
+{
+putchar(letter);
+}
 putchar('\n');
 return (0);
 }
