a13ddd20e89f74e0803e8c20687293ed0a89b08e
Airat <airatsuccess@gmail.com>
2023-02-17 20:20:44 -0800

    4-print_alphabt.c && git push
    git add . && git commit -m

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 char ch;
-for(ch = 'a'; ch <= 'z'; ch++)
-    {   
-        if((ch == 'q') || (ch == 'e'))
-        continue;
-        putchar(ch);
-        
-    }
-    putchar('\n');
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if ((ch == 'q') || (ch == 'e'))
+continue;
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
