950dee0ce6d4f52023ca0d7fd3dce0d30eb56815
Airat <airatsuccess@gmail.com>
2023-02-27 10:01:29 -0800

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,32 +8,19 @@
 void rev_string(char *s)
 {
 int i, j;
-
-	char r[1000];
-
-
-
-	i = j = 0;
-
-	while (s[i] != '\0')
-
-	{
+char r[1000];
+i = j = 0;
+while (s[i] != '\0')
+{
 r[i] = s[i];
-
-		i++;
-
-	}
-
-	i--;
-
-	while (i >= 0)
-
-	{
-         s[i] = r[j];
-
-	 i--;
-	 j++;
-        }
-        s[j++] = '\0';
-
+i++;
+}
+i--;
+while (i >= 0)
+{
+s[i] = r[j];
+i--;
+j++;
+}
+s[j++] = '\0';
 }
